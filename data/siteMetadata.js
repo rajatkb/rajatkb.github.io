7d3a412c88edd4218e0f8eb4d3ffa467d77e7695
rajatkb <rajatthepagal@gmail.com>
2026-06-04 00:28:06 +0530

    hide comments section when Giscus env vars are not set

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -17,22 +17,24 @@ const siteMetadata = {
   locale: 'en-US',
   stickyNav: false,
   analytics: {},
-  comments: {
-    provider: 'giscus',
-    giscusConfig: {
-      repo: process.env.NEXT_PUBLIC_GISCUS_REPO,
-      repositoryId: process.env.NEXT_PUBLIC_GISCUS_REPOSITORY_ID,
-      category: process.env.NEXT_PUBLIC_GISCUS_CATEGORY,
-      categoryId: process.env.NEXT_PUBLIC_GISCUS_CATEGORY_ID,
-      mapping: 'pathname',
-      reactions: '1',
-      metadata: '0',
-      theme: 'light',
-      darkTheme: 'transparent_dark',
-      themeURL: '',
-      lang: 'en',
-    },
-  },
+  comments: process.env.NEXT_PUBLIC_GISCUS_REPO
+    ? {
+        provider: 'giscus',
+        giscusConfig: {
+          repo: process.env.NEXT_PUBLIC_GISCUS_REPO,
+          repositoryId: process.env.NEXT_PUBLIC_GISCUS_REPOSITORY_ID,
+          category: process.env.NEXT_PUBLIC_GISCUS_CATEGORY,
+          categoryId: process.env.NEXT_PUBLIC_GISCUS_CATEGORY_ID,
+          mapping: 'pathname',
+          reactions: '1',
+          metadata: '0',
+          theme: 'light',
+          darkTheme: 'transparent_dark',
+          themeURL: '',
+          lang: 'en',
+        },
+      }
+    : null,
   search: {
     provider: 'kbar',
     kbarConfig: {
